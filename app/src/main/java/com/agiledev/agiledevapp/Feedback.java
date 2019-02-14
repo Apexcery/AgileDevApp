f08026a2fa258b8f4e4c713e1e6875e727bd2850
s6104158 <s6104158>
2019-02-14 15:19:50 +0000

    moved the feedback page to its own activity and added a give feedback button to help fragment. need to added funtionality to submit button and cancel button on feedback activity.

---
+++ app/src/main/java/com/agiledev/agiledevapp/Feedback.java
@@ -0,0 +1,32 @@
+package com.agiledev.agiledevapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import static android.app.PendingIntent.getActivity;
+
+public class Feedback extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+        /*Button cancelbutton = (Button) findViewById(R.id.Submitbutton);*/
+        /*cancelbutton.setOnClickListener*/
+
+    }
+
+    /*@Override
+    public void onClick(View v)
+    {
+        finish();
+    }*/
+}
--- app/src/main/java/com/agiledev/agiledevapp/HelpFragment.java
+++ app/src/main/java/com/agiledev/agiledevapp/HelpFragment.java
@@ -1,6 +1,7 @@
 package com.agiledev.agiledevapp;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -13,13 +14,28 @@ import android.widget.EditText;
  * Created by s6104158 on 07/02/19.
  */
 
-public class HelpFragment extends Fragment {
+public class HelpFragment extends Fragment implements View.OnClickListener {
 
     View myView;
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+    {
         myView = inflater.inflate(R.layout.fragment_help, container, false);
+        Button feedbackbutton = (Button) myView.findViewById(R.id.FeedbackButton);
+        feedbackbutton.setOnClickListener(this);
         return myView;
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.FeedbackButton:
+                Intent intent = new Intent(getActivity(), Feedback.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
