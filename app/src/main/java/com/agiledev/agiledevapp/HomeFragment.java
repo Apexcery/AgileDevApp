18775628224e9d0a9182fa3764d1e7939faa8a05
s6104158 <s6104158>
2019-02-26 15:58:49 +0000

    changed help page added more text added icon. TODO finish home fragment into tabs

--- app/src/main/java/com/agiledev/agiledevapp/HomeFragment.java
+++ app/src/main/java/com/agiledev/agiledevapp/HomeFragment.java
@@ -1,8 +1,10 @@
 package com.agiledev.agiledevapp;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +19,18 @@ public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+    {
         myView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //TabAdapter adapter = new TabAdapter(getActivity().getSupportFragmentManager());
+        TabAdapter Adapter = new TabAdapter(((FragmentActivity) getActivity()).getSupportFragmentManager());
+        Adapter.addFragment(new LoginFragment(), "Login");
+        Adapter.addFragment(new RegisterFragment(), "Register");
+        //viewPager.setAdapter(adapter);
+        //tabLayout.setupWithViewPager(viewPager);
+
         return myView;
     }
+
 }
