aaa2a786a98674fa02b0d80c82f5a9796c66fb1d
s6284463 <xcegla@gmail.com>
2019-04-02 15:56:08 +0100

    Access to DB in settings is granted. Added user email in LoginRegisterActivity

--- app/src/main/java/com/agiledev/agiledevapp/SettingsFragment.java
+++ app/src/main/java/com/agiledev/agiledevapp/SettingsFragment.java
@@ -10,6 +10,8 @@ import android.preference.ListPreference;
 
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
@@ -23,8 +25,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 //public class SettingsFragment extends android.support.v4.app.Fragment
@@ -46,6 +57,8 @@ public class SettingsFragment extends PreferenceFragmentCompat
     SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 //    @Override
 //   public void onCreate(Bundle savedInstanceState) {
 //
@@ -54,30 +67,6 @@ public class SettingsFragment extends PreferenceFragmentCompat
 //
 //
 //    }
-//      final CheckBoxPreference checkboxPref = (CheckBoxPreference) getPreferenceManager().findPreference("key_checkbox1");
-//
-//
-//        SharedPreferences.OnSharedPreferenceChangeListener spChanged = new
-//                SharedPreferences.OnSharedPreferenceChangeListener() {
-//                    @Override
-//                    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-//                                                          String key) {
-//                        // your stuff here
-//                    }
-//                };
-
-
-//        };
-
-     //   CheckBoxPreference c = new CheckBoxPreference();
-    //    Button b = new Button();
-   //     CheckBoxPreference box =   ((SettingsFragment)getParentFragment()).findPreference("key_checkbox1");
-
-//        for(int i = 0; i < pref.size(); i++)
-//       {
-//            pref.get(i).setSummary("key_passwordkey_password");
-//        }
-//
     /*
     private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
         @Override
@@ -108,39 +97,166 @@ public class SettingsFragment extends PreferenceFragmentCompat
         editor = sharedPref.edit();
 
 
-        android.support.v7.preference.EditTextPreference userNamePreference = (android.support.v7.preference.EditTextPreference) findPreference("key_changeUsername");
-        android.support.v7.preference.EditTextPreference userEmailPreference = (android.support.v7.preference.EditTextPreference) findPreference("key_changeEmail");
+
+        final android.support.v7.preference.EditTextPreference userNamePreference = (android.support.v7.preference.EditTextPreference) findPreference("key_changeUsername");
+        final android.support.v7.preference.EditTextPreference userEmailPreference = (android.support.v7.preference.EditTextPreference) findPreference("key_changeEmail");
+        final android.support.v7.preference.EditTextPreference userPasswordPreference = (android.support.v7.preference.EditTextPreference) findPreference("key_changePassword");
 
         final CheckBoxPreference checkboxPref = (CheckBoxPreference) getPreferenceManager().findPreference("key_checkbox1");
+        final CheckBoxPreference checkboxPref2 = (CheckBoxPreference) getPreferenceManager().findPreference("key_checkbox_newEpisode");
+        final CheckBoxPreference checkboxPref3 = (CheckBoxPreference) getPreferenceManager().findPreference("key_checkbox_trailerN");
+
 
         //default text of change username pop up settings is the current username
-        userNamePreference.setText(getString(R.string.empty_string, sharedPref.getString(getString(R.string.prefs_loggedin_username),"Default value")));
-      //  userEmailPreference.setText(getString(R.string.empty_string, sharedPref.getString(getString(R.string.prefs_loggedin_email),"Default value")));
-      //  userEmailPreference.setText(getString(R.string.empty_string, sharedPref.getString(getString(R.string.prefs_loggedin_email),"Default value")));
+      //  userNamePreference.setText(getString(R.string.empty_string, sharedPref.getString(getString(R.string.prefs_loggedin_username),"Default value")));
+//        String newUsername = getString(R.string.empty_string, sharedPref.getString(getString(R.string.prefs_loggedin_username),"Default value"));
+//        userNamePreference.setDefaultValue(userNamePreference);
+//        editor.remove("loggedInUsername");
+//        editor.putString("loggedInUsername", newUsername);
+//        editor.apply();
+
+         String oldUsername = getString(R.string.empty_string, sharedPref.getString(getString(R.string.prefs_loggedin_username),"Default value")); //jj
+        userNamePreference.setDefaultValue(oldUsername); // jj initially
+
+        String newUsername = userNamePreference.getText(); // whatever is in the preference after you click submit
+
+        editor.remove("loggedInUsername");
+        editor.putString("loggedInUsername", newUsername); // in shared preferences, replace old username with the new one
+        editor.apply();
+                             //find the old user
+        replaceDetails(db, oldUsername, newUsername, userEmailPreference); // find user in db and replace details
+
+        //logged in as : 'new username' [only changes it after clicking on settings the second time]
+
+        NavigationView navigationView = this.getActivity().findViewById(R.id.nav_view);
+       // navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+        TextView textView = navigationView.getHeaderView(0).findViewById(R.id.loggedInUser);
+        textView.setText(getString(R.string.nav_loggedin_as, sharedPref.getString(getString(R.string.prefs_loggedin_username),"Error, user not found!")));
+
+        //default value = current logged in as: "this"
+   //     userNamePreference.setDefaultValue(getString(R.string.empty_string, sharedPref.getString(getString(R.string.prefs_loggedin_username),"Default value")));
+      //  userNamePreference.setDefaultValue(userNamePreference);
+     //   userNamePreference.setDefaultValue(newUsername);
+   //    pullInformation(db);
+
+  // find where when the app starts it sets the loggedInUsername \/ as text from shared preferences and call it here so you don't have to restart the app for it to change
 
 
-     //   userNamePreference.setText(sharedPref.getString(String.valueOf(R.string.prefs_loggedin_username), "defautl value")); // default value is the current username
-     //  userNamePreference.setDefaultValue(sharedPref.getString(String.valueOf(R.string.prefs_loggedin_username).toString(), "d"));
+
+
+      //  checkboxPref.setSummary(newUsername);
+
+//        onClickPreference.setOnPreferenceClickListener(EditTextPreference f -> {
+//            // do something
+//            return true;
+//        });
+
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("dob", txtDoB.getText().toString());
+//        user.put("email", txtEmail.getText().toString());
+//        user.put("password", hashedPass);
+//
+//        RegisterFragment.registerUser(newUsername, user);  // changed registerUser to static and private -> public
+////
+    //   checkboxPref.setSummary(userNamePreference.getText());
 
 
 
-    //    textView.setText(getString(R.string.nav_loggedin_as, sharedPref.getString(getString(R.string.prefs_loggedin_username),"Error, user not found!")));
 
-      //  userNamePreference.setDefaultValue(sharedPref.getString(R.string.prefs_loggedin_username));
+//        if(checkboxPref.isChecked())
+//        { // stay logged in settings checkbox
+//            // stay logged in
+//        } else {
+//            // log user off after exiting the app
+//        }
+//
+//        if(checkboxPref2.isChecked())
+//        { // new episode notifications
+//            // send the user notifications if new episode of currently watched show comes out
+//        } else {
+//            // do nothing, don't send any notification
+//        }
+//
+//        if(checkboxPref3.isChecked())
+//        { // new trailer notifications
+//            // send the user notifications if new trailer of currently watched show comes out
+//        } else {
+//            // do nothing, don't send any notification
+//        }
 
+//      PreferenceFragmentCompat.setOnPreferenceChangeListener(
+//        @Override
+//        public boolean onPreferenceChange(Preference preference, Object newValue) {
+//        Log.e("preference", "Pending Preference value is: " + newValue);
+//        return true;
+//    }
+//      );
 
-        //sharedPref.getString(getString(R.string.prefs_loggedin_username)
-      //  sharedPref.getString(getString(R.string.prefs_loggedin_username))
-
-     //   checkboxPref.setSummary(String.valueOf(changUss.getText().toString()));
-
-     //   System.out.println(String.valueOf(changUss.getText().toString()));
-
-       // checkboxPref.setSummary("suumma");
-        // changUss.setSummary("suumma");
-
-            //password goes into fb
-        //to do username create onclick listener here, when accept is clicked new create new shared preference and replace/add new username from textfield string
-       // loginergisteractivity.java = see
     }
+                                                                                                                        //CheckBoxPreference just for testing
+    public static synchronized void replaceDetails(FirebaseFirestore db, final String oldUsername, final String newUsername, final android.support.v7.preference.EditTextPreference pref) {
+        db.collection("UserDetails").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots)
+            {
+                for (QueryDocumentSnapshot document : queryDocumentSnapshots)
+                {
+                    if(document.getId().equals(oldUsername)) // get the email and password of currently logged in user
+                    {
+                        ///////
+//                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//                        db.collection("UserDetails").document(username).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                Log.d("Success", "DocumentSnapshot successfully written!");
+//                            }
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w("Error", "Error writing document", e);
+//                            }
+//                        });
+
+                        ///
+
+
+                      String oldEmail = document.getData().get("email").toString();
+                      String oldPassWord = document.getData().get("password").toString();
+pref.setText(oldEmail);
+                        for ( int i = 0; i <   LoginRegisterActivity.userList.size(); i++)
+                        {
+                            if(LoginRegisterActivity.userList.get(i).getUsername().equals(oldUsername))
+                            { // delete the user with old username
+                                LoginRegisterActivity.userList.remove(LoginRegisterActivity.userList.get(i));
+                                //delete from db
+                           //     deleteUser(oldUsername, LoginRegisterActivity.userList.get(i));
+                            }
+                        }
+                     // adding the same user with changed username if there were any
+                      LoginRegisterActivity.User user = new LoginRegisterActivity.User();
+                 // if user details are first changed in firebase then pull from them by using      user.setUsername(document.getId()); [if not just user newUsername
+                      user.setUsername(newUsername);
+                      user.setPassword(document.getData().get("password").toString());
+                      user.setEmail(document.getData().get("email").toString()); ////
+                      LoginRegisterActivity.userList.add(user);
+                    }
+//                    Log.e("Found Username", document.getId());
+//                    Log.e("Found Password", document.getData().get("password").toString());
+//                    Log.e("Found Email", document.getData().get("email").toString()); ////
+//
+//                    LoginRegisterActivity.User user = new LoginRegisterActivity.User();
+//                    user.setUsername(document.getId());
+//                    user.setPassword(document.getData().get("password").toString());
+//                    user.setEmail(document.getData().get("email").toString()); ////
+//
+//                    userList.add(user);
+//                    //  Log.e("Saved User", "Username: " + user.getUsername() + " | Password: " + user.getPassword());
+//                    Log.e("Saved User", "Username: " + user.getUsername() + " | Password: " + user.getPassword() + " | Email: " + user.getEmail());
+//                    Log.e("-",":-");
+                }
+            }
+        });
+    }
+
 }
